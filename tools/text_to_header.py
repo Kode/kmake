59bdfb4cb9500e324753f7cce7d6678c0f7d18e2
Robert Konrad <git2016@robdangero.us>
2022-02-14 00:40:36 +0100

    Preserve line-endings in their original form when embedding text-files

--- tools/text_to_header.py
+++ tools/text_to_header.py
@@ -3,14 +3,14 @@
 import sys
 
 def main():
-	input_file = open(sys.argv[2], "r")
-	text = input_file.read()
+	input_file = open(sys.argv[2], "rb")
+	text = input_file.read().decode()
 	input_file.close()
 
-	output_file = open(sys.argv[3], "w")
-	output_file.write("static const char " + sys.argv[1] + "[] = \"")
-	output_file.write(text.replace("\\", "\\\\").replace("\n", "\\n").replace("\r", "\\r").replace("\t", "\\t").replace("\"", "\\\""))
-	output_file.write("\";\n")
+	output_file = open(sys.argv[3], "wb")
+	output_file.write(("static const char " + sys.argv[1] + "[] = \"").encode("utf8"))
+	output_file.write(text.replace("\\", "\\\\").replace("\n", "\\n").replace("\r", "\\r").replace("\t", "\\t").replace("\"", "\\\"").encode("utf8"))
+	output_file.write("\";\n".encode("utf8"))
 	output_file.close()
 
 if __name__ == "__main__":
