a21b8c4781a1a6e810923755fb9748f95ac065ff
Robert Konrad <git2016@robdangero.us>
2022-10-16 03:12:12 +0200

    Create the Deployment-dir in --init

--- kmake/src/init.ts
+++ kmake/src/init.ts
@@ -18,7 +18,9 @@ export function run(name: string, from: string) {
 		{ encoding: 'utf8' });
 	}
 
-	if (!fs.existsSync(path.join(from, 'Sources'))) fs.mkdirSync(path.join(from, 'Sources'));
+	if (!fs.existsSync(path.join(from, 'Sources'))) {
+		fs.mkdirSync(path.join(from, 'Sources'));
+	}
 
 	let friendlyName = name;
 	friendlyName = friendlyName.replace(/ /g, '_');
@@ -30,5 +32,9 @@ export function run(name: string, from: string) {
 			+ '\treturn 0;\n'
 			+ '}\n';
 		fs.writeFileSync(path.join(from, 'Sources', 'main.c'), mainsource, { encoding: 'utf8' });
+	}
+
+	if (!fs.existsSync(path.join(from, 'Deployment'))) {
+		fs.mkdirSync(path.join(from, 'Deployment'));
 	}
 }
