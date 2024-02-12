4e1713c975712c78521ca5a8d3aab6ef14affc4f
Robert Konrad <git2016@robdangero.us>
2024-02-12 17:26:58 +0100

    Work on the glob

---
+++ glob/src/dir.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct directory {
+	void *handle;
+} directory;
+
+typedef struct file {
+	bool valid;
+	char name[256];
+} file;
+
+directory open_dir(const char *dirname);
+file read_next_file(directory *dir);
+void close_dir(directory *dir);
+
+#ifdef __cplusplus
+}
+#endif
---
+++ glob/src/glob.cpp
@@ -0,0 +1,36 @@
+#include "dir.h"
+#include "log.h"
+
+#include <string>
+
+const char *pattern = "../../kmake/**";
+
+using std::string;
+
+string find_base_dir(const string &pattern) {
+	size_t last_break = pattern.size();
+	for (size_t i = 0; i < pattern.size(); ++i) {
+		if (pattern[i] == '/' || pattern[i] == '\\') {
+			last_break = i;
+		}
+		else if (pattern[i] == '*') {
+			return pattern.substr(0, last_break);
+		}
+	}
+	return pattern;
+}
+
+string to_absolute(const string &path) {
+	return "";
+}
+
+int main(int argc, char **argv) {
+	string base = find_base_dir(pattern);
+	directory dir = open_dir(base.c_str());
+	file f = read_next_file(&dir);
+	while (f.valid) {
+		kmake_log(LOG_LEVEL_INFO, "Visited %s", f.name);
+		f = read_next_file(&dir);
+	}
+	return 0;
+}
